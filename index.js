4d23b1aaa98f01f1f68b4319e1d13905e506abd5
SerejkaP <haggardspark@gmail.com>
2020-07-03 18:16:23 +0600

    create some calculating operations for two arguments

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log(‘Hello World!’)
+// подключение express
+const express = require('express');
+const bodyParser = require('body-parser');
+// создаем объект приложения
+const app = express();
+// Подключаем body-parser
+app.use(bodyParser.json());
+
+// Определяем обработчик корневого урла с методом POST
+app.post("/", function(request, response) {
+    const { firstArg, secondArg, operation } = request.body; // деструктуризация
+    switch(operation){
+        case '+':
+            response.status(200).json(firstArg + secondArg);
+            break;
+        case '-':
+            response.status(200).json(firstArg - secondArg);
+            break;
+        case '*':
+            response.status(200).json(firstArg * secondArg);
+            break;
+        case '/':
+            response.status(200).json(firstArg / secondArg);
+            break;
+        case '^':
+            response.status(200).json(Math.pow(firstArg, secondArg));
+            break;
+        case 'sqrt':
+            response.status(200).json(Math.sqrt(firstArg, secondArg));
+            break;
+        case '%':
+            response.status(200).json((firstArg / secondArg) * 100 + '%');
+            break;
+    }
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
