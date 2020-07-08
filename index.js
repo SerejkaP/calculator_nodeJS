ede13d03314c4501fb501f719a1b74146bb99eef
SerejkaP <haggardspark@gmail.com>
2020-07-08 16:23:40 +0600

    add operations with one argument

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Подключаем body-parser
 app.use(bodyParser.json());
 
-// Определяем обработчик корневого урла с методом POST
+// Операции с двумя аргументами
 app.post("/", function(request, response) {
     const { firstArg, secondArg, operation } = request.body; // деструктуризация
     switch(operation){
@@ -26,10 +26,28 @@ app.post("/", function(request, response) {
             response.status(200).json(Math.pow(firstArg, secondArg));
             break;
         case 'sqrt':
-            response.status(200).json(Math.sqrt(firstArg, secondArg));
+            response.status(200).json(Math.pow(firstArg, 1/secondArg));
             break;
         case '%':
-            response.status(200).json((firstArg / secondArg) * 100 + '%');
+            response.status(200).json(firstArg * (secondArg / 100));
+            break;
+    }
+});
+// Операции с одним аргументом
+app.post("/one-argument", function(request, response) {
+    const { firstArg, operation } = request.body; // деструктуризация
+    switch(operation){
+        case 'sin':
+            response.status(200).json(Math.sin(firstArg));
+            break;
+        case 'cos':
+            response.status(200).json(Math.cos(firstArg));
+            break;
+        case 'tg':
+            response.status(200).json(Math.tg(firstArg));
+            break;
+        case 'ctg':
+            response.status(200).json(Math.ctg(firstArg));
             break;
     }
 });
