ebf6f78f7595d496c6016fe80314b4a7fd279747
SerejkaP <haggardspark@gmail.com>
2020-07-12 20:49:41 +0600

    add checking for numeric arguments

--- index.js
+++ index.js
@@ -8,23 +8,34 @@ app.use(bodyParser.json());
 
 app.post("/two-args", function(request, response) {
     const { firstArg, secondArg, operation } = request.body;
-    try {
-        response
-            .status(200)
-            .json(getTwoArgsOperation(operation)(firstArg, secondArg));
-    } catch (e) {
+    var firArg = Number(firstArg);
+    var secArg = Number(secondArg);
+    if ( Number.isNaN(firArg) || Number.isNaN(secArg)) {
+        response.status(400).json('There are not all numeric arguments here.');
+    } else {
+        try {
+            response
+                .status(200)
+                .json(getTwoArgsOperation(operation)(firstArg, secondArg));
+        } catch (e) {
         response.status(400).json(e.message);
+        }
     }
 });
 
 app.post("/one-arg", function(request, response) {
     const { oneArg, operation } = request.body;
-    try {
-        response
-            .status(200)
-            .json(getOneArgOperation(operation)(oneArg));
-    } catch (e) {
-        response.status(400).json(e.message);
+    var arg = Number(oneArg);
+    if (Number.isNaN(arg)) {
+        response.status(400).json('It is not a numeric argument here.');
+    } else {
+        try {
+            response
+                .status(200)
+                .json(getOneArgOperation(operation)(oneArg));
+        } catch (e) {
+            response.status(400).json(e.message);
+        }
     }
 });
 
